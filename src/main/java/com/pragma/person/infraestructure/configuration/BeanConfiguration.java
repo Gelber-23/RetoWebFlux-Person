ce6429bd042246a088603f09c8cb0ce405ac9a12
Gelber-23 <gemartinezpas@gmail.com>
2025-07-23 10:35:57 -0500

    Added comunication to report

--- src/main/java/com/pragma/person/infraestructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/person/infraestructure/configuration/BeanConfiguration.java
@@ -3,12 +3,15 @@ package com.pragma.person.infraestructure.configuration;
 
 import com.pragma.person.domain.api.IPersonServicePort;
 import com.pragma.person.domain.spi.IPersonPersistencePort;
+import com.pragma.person.domain.spi.web.IBootcampClientPort;
+import com.pragma.person.domain.spi.web.IReportClientPort;
 import com.pragma.person.domain.usecase.PersonUseCase;
 import com.pragma.person.infraestructure.output.jpa.adapter.PersonJpaAdapter;
 import com.pragma.person.infraestructure.output.jpa.mapper.IPersonEntityMapper;
 import com.pragma.person.infraestructure.output.jpa.repository.IPersonBootcampRepository;
 import com.pragma.person.infraestructure.output.jpa.repository.IPersonEntityRepository;
 import com.pragma.person.infraestructure.output.webclient.BootcampWebClientAdapter;
+import com.pragma.person.infraestructure.output.webclient.ReportWebClientAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +24,8 @@ public class BeanConfiguration {
     private final IPersonEntityRepository personEntityRepository;
     private final IPersonEntityMapper personEntityMapper;
     private final IPersonBootcampRepository personBootcampRepository;
-    private final BootcampWebClientAdapter bootcampWebClientAdapter;
-
+    private final IBootcampClientPort bootcampWebClientAdapter;
+    private final IReportClientPort reportWebClientAdapter;
     @Bean
     public IPersonPersistencePort personPersistencePort(){
         return new PersonJpaAdapter(personEntityRepository, personBootcampRepository, personEntityMapper,bootcampWebClientAdapter);
@@ -30,6 +33,6 @@ public class BeanConfiguration {
 
     @Bean
     public IPersonServicePort personServicePort(){
-        return new PersonUseCase(personPersistencePort(),bootcampWebClientAdapter);
+        return new PersonUseCase(personPersistencePort(),bootcampWebClientAdapter,reportWebClientAdapter);
     }
 }
